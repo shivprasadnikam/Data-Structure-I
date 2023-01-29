b10d8e26cb188753fc27ef1a913c96421799d644
Shivprasad Nikam <116971586+shivprasadnikam@users.noreply.github.com>
2023-01-29 19:26:22 +0530

    Create TwoSum.cpp

---
+++ Day 02/TwoSum.cpp
@@ -0,0 +1,24 @@
+
+                                                              // Two Sum  //
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int sum=0;
+        vector<int> ans;
+        
+        for(int i=0; i<nums.size()-1; i++)
+        {
+            for(int j=i+1; j<nums.size(); j++)
+            {
+                sum=nums[i]+nums[j];
+                if(sum==target)
+                {
+                    ans.push_back(i);
+                    ans.push_back(j);
+                }
+            }
+        }
+        return ans;
+    }
+};
